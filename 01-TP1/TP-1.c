982a0f66a7431268edd2d02284a48eb99a52dd9e
FRANCISCO OU SU <129127893+FranciscoOu@users.noreply.github.com>
2023-05-11 16:26:31 -0300

    Update TP-1.c

--- 01-TP1/TP-1.c
+++ 01-TP1/TP-1.c
@@ -1,157 +1,93 @@
 #include <stdio.h>
 
-int longitudCadena(char *cadena) {
-    int i = 0;
-    while (*cadena != '\0') {  // mientras no se llegue al final de la cadena ('\0')
-        i++;                  // incrementar variable de longitud
-        cadena++;             // avanzar al siguiente carácter de la cadena
+// a) Calcular y retornar la longitud de una cadena dada.
+int calcularLongitud(const char *cadena) {
+    int longitud = 0;
+    while (*cadena != '\0') {
+        longitud++;
+        cadena++;
     }
-    return i;
+    return longitud;
 }
 
-void convCadenaANumerico(char* cadena){
-    int i, num;
-    while (cadena[i] != '\0') {
-            num *= 10; // multiplicamos por 10 para mover el dígito anterior un lugar a la izquierda
-            num += cadena[i] - '0'; // sumamos el valor numérico del carácter actual al número entero
-            i++;
+// b) Convertir una cadena de dígitos en su equivalente numérico.
+int convertirANumero(const char *cadena) {
+    int numero = 0;
+    while (*cadena != '\0') {
+        numero = numero * 10 + (*cadena - '0');
+        cadena++;
     }
+    return numero;
 }
 
-void convCadenaAMayuscula(char* str) {
-    int i = 0;
-
-    while(str[i] != '\0') { // Iterar sobre cada caracter de la cadena
-        if(str[i] >= 'a' && str[i] <= 'z') { // Si el caracter es una letra minúscula
-            str[i] = str[i] - 'a' + 'A'; // Convertir a mayúscula
+// c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula.
+void convertirAMayusculas(char *cadena) {
+    while (*cadena != '\0') {
+        if (*cadena >= 'a' && *cadena <= 'z') {
+            *cadena = *cadena - 32;
         }
-        i++; // Incrementar el índice
+        cadena++;
     }
 }
 
-void eliminar_caracter(char* cadena, char caracter_a_eliminar) {
-    int i, j;
-
-    // Recorremos la cadena de caracteres
-    for (i = 0; cadena[i] != '\0'; i++) {
-        // Si encontramos el carácter a eliminar, reorganizamos la cadena
-        if (cadena[i] == caracter_a_eliminar) {
-            // Recorremos el resto de la cadena hacia la izquierda
-            for (j = i; cadena[j] != '\0'; j++) {
-                cadena[j] = cadena[j + 1];
-            }
-            // Al eliminar un carácter, mantenemos la posición del índice en el mismo lugar, para no saltarnos caracteres
-            i--;
+// d) Eliminar de una cadena dada todas las ocurrencias de un carácter dado.
+void eliminarCaracter(char *cadena, char caracter) {
+    char *destino = cadena;
+    while (*cadena != '\0') {
+        if (*cadena != caracter) {
+            *destino = *cadena;
+            destino++;
         }
+        cadena++;
+    }
+    *destino = '\0';
+}
+
+// e) Concatenar al final de la primera cadena dada una segunda cadena también dada.
+void concatenarCadenas(char *cadena1, const char *cadena2) {
+    while (*cadena1 != '\0') {
+        cadena1++;
+    }
+    while (*cadena2 != '\0') {
+        *cadena1 = *cadena2;
+        cadena1++;
+        cadena2++;
+    }
+    *cadena1 = '\0';
+}
+
+// f) Modificar la cadena dada con la inserción de un carácter dado en una posición determinada.
+void insertarCaracter(char *cadena, char caracter, int posicion) {
+    int longitud = calcularLongitud(cadena);
+    if (posicion >= 0 && posicion <= longitud) {
+        for (int i = longitud; i >= posicion; i--) {
+            cadena[i + 1] = cadena[i];
+        }
+        cadena[posicion] = caracter;
     }
 }
 
-void concatenar(char* cadena1, char* cadena2) {
-    // Encontrar la longitud de la primera cadena
-    int longitud = longitudCadena(cadena1);
+int main() {
+    char cadena1[100] = "Hola";
+    char cadena2[] = "Mundo";
+    char cadena3[] = "12345";
+    char cadena4[] = "AbcdefAbcdef";
+    char cadena5[100] = "Texto";
 
-    // Concatenar la segunda cadena al final de la primera cadena
-    int i = longitud;
-    int j = 0;
-    
-    // Encontrar la longitud de cadena1
-    for (i = 0; cadena1[i] != '\0'; i++);
-    
-    // Concatenar cadena2 al final de cadena1
-    for (j = 0; cadena2[j] != '\0'; j++) {
-        cadena1[i] = cadena2[j];
-        i++;
-    }
-    
-    cadena1[i] = '\0'; // Agregar el caracter nulo al final de la cadena1
-}
+    printf("Longitud de la cadena '%s': %d\n", cadena1, calcularLongitud(cadena1));
+    printf("Número equivalente de la cadena '%s': %d\n", cadena3, convertirANumero(cadena3));
 
-void modificaCaracter(char* cadena, int posicion, char caracter){
-    // Encontrar la longitud de la primera cadena
-    int longitud_cadena = longitudCadena(cadena);
+    convertirAMayusculas(cadena1);
+    printf("Cadena en mayúsculas: %s\n", cadena1);
 
-    for (int i = longitud_cadena; i >= posicion; i--) {
-    cadena[i+1] = cadena[i];
-    }
-    cadena[posicion] = caracter;
-}
+    eliminarCaracter(cadena4, 'b');
+    printf("Cadena sin ocurrencias de 'b': %s\n", cadena4);
 
+    concatenarCadenas(cadena1, cadena2);
+    printf("Concatenación de cadenas: %s\n", cadena1);
 
-int main()
-{
-/*
-    1) Realizar en Lenguaje C una función para realizar cada una de las operaciones solicitadas, sin la utilización de funciones de biblioteca:  
-*/
-    //a) Calcular y retornar la longitud de una cadena dada.
-    char cadena[] = "hola mundo";
-    int longitud = longitudCadena(cadena);
-    printf("La longitud de la cadena '%s' es %d\n", cadena, longitud); // imprimir la longitud de la cadena
-    printf("\n");
-
-    //b) Convertir una cadena de dígitos en su equivalente numérico. 
-    char str[] = "hola"; // cadena de caracteres a convertir
-    int numero;
-
-    printf("Cadena original: %s\n", str);
-
-    convCadenaANumerico(str);
-
-    printf("El número resultante es: %d\n", numero);
-    printf("\n");
-
-    //c) Convertir una cadena de caracteres a la misma cadena con todos sus caracteres en mayúscula. 
-    char string[] = "hola mundo";
-
-    printf("Cadena original: %s\n", string);
-
-    convCadenaAMayuscula(string); // Convertir la cadena a mayúsculas
-
-    printf("La cadena en mayusculas es: %s\n", string); // Imprimir la cadena convertida
-    printf("\n");
-
-    //d) Eliminar de una cadena dada todas las ocurrencias de un carácter dado. 
-    char caracter_a_eliminar = 'o';
-
-    printf("Cadena original: %s\n", cadena);
-
-    eliminar_caracter(cadena, caracter_a_eliminar);
-
-    printf("Cadena sin el caracter '%c': %s\n", caracter_a_eliminar, cadena);
-    printf("\n");
-
-    //e)Concatenar al final de la primera cadena dada una segunda cadena también dada.
-    // Definir dos cadenas de caracteres
-    char cadena1[20] = "Hola ";
-    char cadena2[20] = "mundo!";
-
-    printf("Primera Cadena: %s \nSegunda Cadena: %s\n", cadena1, cadena2);
-    concatenar(cadena1, cadena2);
-    
-    printf("Cadena concatenada: %s", cadena1);
-    printf("\n\n");
-
-    //f)Modificar la cadena dada con la inserción de un carácter dado en una posición determinada. 
-    // Primero, pedimos introduzca la cadena de caracteres
-    printf("Ingrese una cadena de caracteres: ");
-    char cadena3[100];
-    scanf("%s", cadena3);
-
-    // Luego, solicitamos que elija la posición donde desea insertar el caracter
-    printf("Ingrese la posicion donde desea insertar el caracter: ");
-    int posicion;
-    scanf("%d", &posicion);
-
-    // Después, preguntamos al usuario qué caracter desea insertar
-    printf("Ingrese el caracter que desea insertar: ");
-    char caracter;
-    scanf(" %c", &caracter);
-
-    modificaCaracter(cadena3, posicion, caracter);
-
-    // Finalmente, imprimimos la cadena modificada
-    printf("La cadena modificada es: %s", cadena3);
-
-    
+    insertarCaracter(cadena5, '!', 5);
+    printf("Cadena con carácter insertado: %s\n", cadena5);
 
     return 0;
 }
